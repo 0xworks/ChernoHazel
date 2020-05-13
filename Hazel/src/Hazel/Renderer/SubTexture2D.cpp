d5d0123634cd661bad8aada7a4804ce8d562dff5
freeman40 <jrfwaite@gmail.com>
2020-05-13 20:09:21 +1200

    fix for texture edge bleeding

--- Hazel/src/Hazel/Renderer/SubTexture2D.cpp
+++ Hazel/src/Hazel/Renderer/SubTexture2D.cpp
@@ -12,8 +12,12 @@ namespace Hazel {
 	}
 
 	Ref<SubTexture2D> SubTexture2D::CreateFromCoords(const Ref<Texture2D>& texture, const glm::vec2& coords, const glm::vec2& cellSize, const glm::vec2& spriteSize) {
+		glm::vec2 texelHalfSize = {
+			(1.0f / texture->GetWidth()) * 0.5f,
+			(1.0f / texture->GetHeight()) * 0.5f
+		};
 		glm::vec2 min = {coords.x * cellSize.x / texture->GetWidth(), coords.y * cellSize.y / texture->GetHeight()};
 		glm::vec2 max = {(coords.x + spriteSize.x) * cellSize.x / texture->GetWidth(), (coords.y + spriteSize.y) * cellSize.y / texture->GetHeight()};
-		return CreateRef<SubTexture2D>(texture, min, max);
+		return CreateRef<SubTexture2D>(texture, min + texelHalfSize, max - texelHalfSize);
 	}
 }
