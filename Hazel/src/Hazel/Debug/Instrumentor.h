99adebf97e129804c3d6724ee52d16ead03d9b43
freeman40 <jrfwaite@gmail.com>
2020-06-01 17:19:49 +1200

    Add macros for profiling locks

--- Hazel/src/Hazel/Debug/Instrumentor.h
+++ Hazel/src/Hazel/Debug/Instrumentor.h
@@ -7,10 +7,14 @@
 #define HZ_PROFILE_SCOPE(name) ZoneScopedN(name)
 #define HZ_PROFILE_FUNCTION() ZoneScoped
 #define HZ_PROFILE_FRAMEMARKER() FrameMark
+#define HZ_PROFILE_LOCK(type, var, name) TracyLockableN(type, var, name)
+#define HZ_PROFILE_LOCKMARKER(var) LockMark(var)
 #else
 #define HZ_PROFILE_BEGIN_SESSION(name, filepath)
 #define HZ_PROFILE_END_SESSION()
 #define HZ_PROFILE_SCOPE(name)
 #define HZ_PROFILE_FUNCTION()
 #define HZ_PROFILE_FRAMEMARKER()
+#define HZ_PROFILE_LOCK(type, var, name) type var
+#define HZ_PROFILE_LOCKMARKER(var)
 #endif
